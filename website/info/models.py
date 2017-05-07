046e0fe9e23c19ea07f03a24dda9cb54cf4d47d7
AlanFor301 <allencc26@gmail.com>
2017-05-07 01:14:30 -0600

    fix date warning: for active time, add 'T' sign between day and hour

--- website/info/models.py
+++ website/info/models.py
@@ -7,6 +7,7 @@ from django.db import models
 class Staff(models.Model):
     staff_name = models.CharField(max_length=250)
     branch_location = models.CharField(max_length= 100)
-    available_time = models.DateTimeField()
+    start_time = models.DateTimeField(default = '2016-01-01T10:20:05.123')
+    end_time = models.DateTimeField(default = '2016-01-01T10:20:05.123')
     def __str__(self):
-        return self.staff_name + ' - ' + self.branch_location
+        return self.staff_name + ' - ' + self.branch_location + ' - ' + self.start_time + ' to '+ self.end_time
