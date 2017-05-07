daf9b816a00a1d60725a2df7030e39e042a54674
AlanFor301 <allencc26@gmail.com>
2017-05-06 22:08:42 -0600

    move html in view.py to a template in templates/info/index.html

--- website/info/views.py
+++ website/info/views.py
@@ -3,15 +3,16 @@ from __future__ import unicode_literals
 from models import Staff
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 # Create your views here.
 def index(request):
-    #connect to the database. And get all data in Staff
     all_staff = Staff.objects.all()
-    html = ''
-    for staff in all_staff:
-        url = '/info/' + str(staff.id) +"/"
-        html += '<a href = "'+ url +'">' + staff.staff_name + '</a><br>'
+    #django will look for template folder
+    template = loader.get_template('info/index.html')
+    #information template needs
+    context = {
+        'all_staff': all_staff}
+    return HttpResponse(template.render(context, request))
 
-    return HttpResponse(html)
 def detail(request, staff_id):
     return HttpResponse("<h2>Details for staff id: "+ str(staff_id)+"</h2>")
