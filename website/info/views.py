13a872b0e302d461bca00ae2d4abeab182b79f8b
AlanFor301 <allencc26@gmail.com>
2017-05-06 20:53:47 -0600

    connect to the database and get each staff name in the index page

--- website/info/views.py
+++ website/info/views.py
@@ -1,10 +1,17 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from models import Staff
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>this is your Info page</h1>")
+    #connect to the database. And get all data in Staff
+    all_staff = Staff.objects.all()
+    html = ''
+    for staff in all_staff:
+        url = '/info/' + str(staff.id) +"/"
+        html += '<a href = "'+ url +'">' + staff.staff_name + '</a><br>'
+
+    return HttpResponse(html)
 def detail(request, staff_id):
     return HttpResponse("<h2>Details for staff id: "+ str(staff_id)+"</h2>")
