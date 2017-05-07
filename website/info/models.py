f2c202342642f7fd3736ba06fb8d0d9b1f1ab638
AlanFor301 <allencc26@gmail.com>
2017-05-07 01:30:06 -0600

    date utoffset: datetime.datetime.strptime('24052010', '%d%m%Y').date()

--- website/info/models.py
+++ website/info/models.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+import datetime
 from django.db import models
 
 # Create your models here.
 class Staff(models.Model):
     staff_name = models.CharField(max_length=250)
     branch_location = models.CharField(max_length= 100)
-    start_time = models.DateTimeField(default = '2016-01-01T10:20:05.123')
-    end_time = models.DateTimeField(default = '2016-01-01T10:20:05.123')
+    start_time = models.DateTimeField(default = datetime.datetime.strptime('24052010', '%d%m%Y').date())
+    end_time = models.DateTimeField(default = datetime.datetime.strptime('24052010', '%d%m%Y').date())
     def __str__(self):
         return self.staff_name + ' - ' + self.branch_location + ' - ' + str(self.start_time) + ' to '+ str(self.end_time)
