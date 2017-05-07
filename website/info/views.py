63b55e012a03e4f6049d826302ca636003e296d6
AlanFor301 <allencc26@gmail.com>
2017-05-06 22:20:28 -0600

    use Render to keep code condense and clean and modify detail.html template, and add a 404 msg if the staff does not exist

--- website/info/views.py
+++ website/info/views.py
@@ -3,16 +3,18 @@ from __future__ import unicode_literals
 from models import Staff
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
 # Create your views here.
+
 def index(request):
     all_staff = Staff.objects.all()
-    #django will look for template folder
-    template = loader.get_template('info/index.html')
     #information template needs
-    context = {
-        'all_staff': all_staff}
-    return HttpResponse(template.render(context, request))
+    context = {'all_staff': all_staff}
+    return render(request, 'info/index.html', context)
 
 def detail(request, staff_id):
-    return HttpResponse("<h2>Details for staff id: "+ str(staff_id)+"</h2>")
+    try:
+        staff = Staff.objects.get(pk=staff_id)
+    except Staff.DoesNotExist:
+        raise Http404("staff does not exist!")
+
+    return render(request, 'info/detail.html', {'staff': staff.staff_name})
