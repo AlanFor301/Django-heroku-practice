dfcfbfab5ac9d5eba3a506002900086ab1833e16
AlanFor301 <allencc26@gmail.com>
2017-05-07 01:41:32 -0600

    add log file for learning notes

--- website/info/models.py
+++ website/info/models.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-import datetime
+from datetime import datetime
 from django.db import models
 
 # Create your models here.
 class Staff(models.Model):
     staff_name = models.CharField(max_length=250)
     branch_location = models.CharField(max_length= 100)
-    start_time = models.DateTimeField(default = datetime.datetime.strptime('24052010', '%d%m%Y').date())
-    end_time = models.DateTimeField(default = datetime.datetime.strptime('24052010', '%d%m%Y').date())
+    start_time = models.DateTimeField(default = datetime.today())
+    end_time = models.DateTimeField(default = datetime.today())
     def __str__(self):
         return self.staff_name + ' - ' + self.branch_location + ' - ' + str(self.start_time) + ' to '+ str(self.end_time)
