6b29c36df7aab91bb436c9636fff58d8fec44b0e
AlanFor301 <allencc26@gmail.com>
2017-05-07 01:15:43 -0600

    stringfy date type in __str__

--- website/info/models.py
+++ website/info/models.py
@@ -10,4 +10,4 @@ class Staff(models.Model):
     start_time = models.DateTimeField(default = '2016-01-01T10:20:05.123')
     end_time = models.DateTimeField(default = '2016-01-01T10:20:05.123')
     def __str__(self):
-        return self.staff_name + ' - ' + self.branch_location + ' - ' + self.start_time + ' to '+ self.end_time
+        return self.staff_name + ' - ' + self.branch_location + ' - ' + str(self.start_time) + ' to '+ str(self.end_time)
